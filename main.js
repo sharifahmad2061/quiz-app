1936f14b1956b6b37058a4e9953f97845691df73
sharifahmad <sharifahmad2061@gmail.com>
2018-04-12 14:58:34 +0500

    works on the layout of the second column

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let win
 
 function createWindow() {
     // Create the browser window.
-    win = new BrowserWindow({ width: 900, height: 600 })
+    win = new BrowserWindow({ width: 1000, height: 700 })
 
     //hide the default menu
     win.setMenu(null)
@@ -19,7 +19,7 @@ function createWindow() {
 
     // and load the index.html of the app.
     win.loadURL(url.format({
-        pathname: path.join(__dirname, 'main.html'),
+        pathname: path.join(__dirname, 'homepage.html'),
         protocol: 'file:',
         slashes: true
     }))
