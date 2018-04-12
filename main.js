af2080d5db4991595eeefc5c572e76390f45d3a1
sharifahmad <sharifahmad2061@gmail.com>
2018-04-12 10:19:47 +0500

    adding individual files

--- main.js
+++ main.js
@@ -10,6 +10,9 @@ function createWindow() {
     // Create the browser window.
     win = new BrowserWindow({ width: 800, height: 600 })
 
+    //hide the default menu
+    win.setMenu(null)
+
     // and load the index.html of the app.
     win.loadURL(url.format({
         pathname: path.join(__dirname, 'index.html'),
@@ -18,7 +21,7 @@ function createWindow() {
     }))
 
     // Open the DevTools.
-    win.webContents.openDevTools()
+    // win.webContents.openDevTools()
 
     // Emitted when the window is closed.
     win.on('closed', () => {
