3d42ae953b6e1868b8cf8a7e44c04726c993d27d
sharifahmad <sharifahmad2061@gmail.com>
2018-04-13 12:06:16 +0500

    time-remaining counter starts working

--- homepage.js
+++ homepage.js
@@ -8,12 +8,46 @@ for (let index = 0; index < 90; index++) {
 }
 
 //move to question event listener
-move_select.addEventListener('change',function(){
+move_select.addEventListener('change', function () {
 
 });
 
 // time remaining clock
+let time_el = document.querySelector("#time-ro");
+let countdown;
+function timer() {
+    const now = Date.now();
+    const then = now + (2 * 60 * 1000); //90min * 60sec * 1000ms
+    const diff = (then - now) / 1000;
+    console.log({'diff':diff});
+    displayTimeLeft(diff);
+
+    countdown = setInterval(() => {
+        const secondsLeft = Math.round((then - Date.now()) / 1000);
+        if (secondsLeft < 0) {
+            //clear the interval
+            clearInterval(countdown);
+            //stop the quiz and show the score
+            return;
+        }
+        displayTimeLeft(secondsLeft);
+    }, 1000);
+}
+function displayTimeLeft(seconds) {
+    const min = Math.floor(seconds / 60);
+    const sec = seconds % 60;
+    const display = `${min < 10 ? '0' : ''}${min}:${sec < 10 ? '0' : ''}${sec}`;
+    console.log(display);
+    time_el.textContent = display;
+}
 
 // start button functionality
+let start_btn = document.querySelector('#start-button > button');
+start_btn.addEventListener('click',()=>{
+    timer();
+});
 
 //end button functionality
+
+
+//count the score and show the result
