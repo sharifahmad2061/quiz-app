7bdd3495314fd8f4e2d13b8d0a33a80887966d0c
sharifahmad <sharifahmad2061@gmail.com>
2018-06-19 18:56:53 +0500

    adds event listeners for previous , next and first last buttons

--- homepage.js
+++ homepage.js
@@ -12,28 +12,33 @@ const time_for_c_test = timing_and_marks[file_and_order['file']][0];
 //load the first test and then start displaying it on start button click
 const c_test = file_and_order['file'];
 let test_data = JSON.parse(fs.readFileSync(path.join(__dirname, `/question/${c_test}.json`)));
+test_data = arrayFromDictionary(test_data);
 // console.log(test_data);
 
 //variables
+let current_qs;
+let qs_attempted;
 let q_for_review = [];
+let total_qs = timing_and_marks[file_and_order['file']][1];
 let unattempted_qs = timing_and_marks[file_and_order['file']][1]; //initialize it total marks , each q has 1 mark
 
-//handling the footer data
-let un_qs_el = document.querySelector('#unattempted-qs');
-let qs_ma_4_re = document.querySelector('#qs-marked-for-review');
-un_qs_el.textContent = unattempted_qs;
-qs_ma_4_re.textContent = q_for_review.length;
+
 
 //handling the questions marked for review display element
 let qm_for_review = document.querySelector('#qs-marked-for-review');
 let re_ch = document.querySelector('#re-ch');
 re_ch.addEventListener('change', () => {
-
+    if (current_qs == null || current_qs == undefined) {
+        console.log('hello');
+        return;
+    }
+    q_for_review.push(current_qs);
+    qm_for_review.textContent = q_for_review.length;
 });
 
 //adding options to move to question select
 var move_select = document.querySelector("#move-to-q-s");
-for (let index = 0; index < 90; index++) {
+for (let index = 0; index < total_qs; index++) {
     var option = document.createElement('option');
     option.appendChild(document.createTextNode(`${index}`));
     option.nodeValue = index;
@@ -51,12 +56,17 @@ move_select.addEventListener('change', function () {
 let time_el = document.querySelector("#time-ro");
 let time_al = document.querySelector("#time-al");
 
+//initialy set both clocks to total time
+displayTimeLeft(time_el, time_for_c_test * 60);
+displayTimeLeft(time_al, time_for_c_test * 60);
+
+
 let countdown;
 function timer(minutes) {
     // const now = Date.now();
     let seconds = minutes * 60; //90min * 60sec * 1000ms
     // const diff = (then - now) / 1000;
-    displayTimeLeft(seconds);
+    displayTimeLeft(time_el, seconds);
 
     countdown = setInterval(() => {
         // const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -67,25 +77,25 @@ function timer(minutes) {
             //stop the quiz and show the score
             return;
         }
-        displayTimeLeft(seconds);
+        displayTimeLeft(time_el, seconds);
     }, 1000);
 }
 
-function displayTimeLeft(seconds) {
+function displayTimeLeft(element, seconds) {
     const hour = Math.floor(seconds / 3600);
     seconds %= 3600;
     const min = Math.floor(seconds / 60);
     const sec = seconds % 60;
     const display = `${hour < 10 ? '0' : ''}${hour}:${min < 10 ? '0' : ''}${min}:${sec < 10 ? '0' : ''}${sec}`;
     // console.log(display);
-    time_el.textContent = display;
+    element.textContent = display;
 }
 
 // start button functionality
 let start_btn = document.querySelector('#start-button > button');
 start_btn.addEventListener('click', () => {
     timer(time_for_c_test);
-});
+}, { once: true });
 
 //end button functionality
 let end_btn = document.querySelector('#end-button > button');
@@ -95,9 +105,58 @@ end_btn.addEventListener('click', () => {
 
 //count the score and show the result
 
+//function data into an array for indexing
+function arrayFromDictionary(data) {
+    var r_data = [];
+    for (const key in data) {
+        if (data.hasOwnProperty(key)) {
+            r_data.push({
+                key: key,
+                value: data[key]
+            });
+        }
+    }
+    return r_data;
+}
+
+//event handlers for previous, next, first and last
+let btn_first = document.querySelector('#btn-first');
+let btn_last = document.querySelector('#btn-last');
+let btn_previous = document.querySelector('#btn-previous');
+let btn_next = document.querySelector('#btn-next');
+
+btn_first.addEventListener('click', () => {
+    showNextQuestion(0);
+    current_qs = 0;
+});
+btn_last.addEventListener('click', () => {
+    showNextQuestion(-1);
+    current_qs = total_qs - 1;
+});
+btn_previous.addEventListener('click', () => {
+    showNextQuestion(current_qs - 1);
+    current_qs--;
+});
+btn_next.addEventListener('click', () => {
+    showNextQuestion();
+    current_qs++;
+});
+
+//function for displaying next question and its options
+function showNextQuestion(num) {
+    let num = num || current_qs + 1;
+
+}
+
+//function for manipulating qs left and marked for review
+function manipulateQsLeftAndMarkedForReview() {
+    let un_qs_el = document.querySelector('#unattempted-qs');
+    let qs_ma_4_re = document.querySelector('#qs-marked-for-review');
+    un_qs_el.textContent = unattempted_qs;
+    qs_ma_4_re.textContent = q_for_review.length;
+}
 
 //functions for setting subject and total questions in the header
-
 function setSubject(subject) {
     let el = document.querySelectorAll('#student-info > div')[2].children[1];
     // console.log(el);
@@ -125,9 +184,8 @@ function setSubject(subject) {
             break;
     }
 }
-
 function setTotalQs(total_qs) {
     let el = document.querySelectorAll('#question-info > div')[1].children[1];
     el.textContent = total_qs;
 }
-setTotalQs(30);
+
