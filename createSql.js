361cb9c1a5ef9445cd92b028cceaed510189b00b
sharifahmad <sharifahmad2061@gmail.com>
2019-02-28 23:41:47 +0500

    almost done with all tables except it2

--- createSql.js
+++ createSql.js
@@ -1,13 +1,16 @@
 var fs = require('fs');
-var obj = JSON.parse(fs.readFileSync('question/it2.json', 'utf8'));
-var sql = "INSERT INTO it2 VALUES";
+var obj = JSON.parse(fs.readFileSync('question/Math.json', 'utf8'));
+var sql = "INSERT INTO math VALUES";
+var index = 0;
 for (const key in obj) {
+    if (key == "urdu") continue;
     if (obj.hasOwnProperty(key)) {
         const values = obj[key];
-        // sql += `("${key}","${values[0]}","${values[1]}","${values[2]}","${values[3]}",${values[4]}),`;
-        sql += `("${key}",${values[4]}),`
+        sql += `("${key}","${values[0]}","${values[1]}","${values[2]}","${values[3]}",${values[4]},"${obj['urdu'][index]}"),`;
+        index++;
+        // sql += `("${key}",${values[4]}),`
     }
 }
 sql = sql.slice(0, -1);
 sql += ";"
-fs.writeFileSync("it2.sql", sql);
+fs.writeFileSync("math.sql", sql);
